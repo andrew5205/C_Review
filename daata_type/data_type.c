87aaae22c15a4e99ecafecbd1d64c056a848c579
Andrew5205 <andrewchung11@gmail.com>
2021-01-31 11:52:34 -0800

    add data_type.c

---
+++ daata_type/data_type.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+
+#include <stdbool.h>
+
+// int 
+// float 
+// double - same type as float, but only roughly twice the precision
+// char 
+// _Bool - only 0 or 1, False or True
+// bool - #include <stdbool.h>
+
+// short 
+// long
+// unsigned - only for nonnegative value
+
+
+int main()
+{
+    // 3 * 10 ^ 8
+    float c = 3e+8;
+    printf("Speed of light is %f m/s", c);
+
+    // bool - #include <stdbool.h>
+    bool myBoolVariable = true;
+
+    // long have l or L immediately followed by 
+    long int number_of_Points = 182930182L;
+
+    long double US_credit_2021 = 1.234e+7L;
+
+    // unsigned - only for nonnegative value
+    unsigned int counter;
+
+}
+
+
+
+
+
+
+
+
+
