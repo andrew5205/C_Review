11ed0ffce89c354784da3486341ab8d22eebde66
Andrew5205 <andrewchung11@gmail.com>
2021-02-02 21:17:40 -0800

    add basic_operator.c

---
+++ operator/basic_operator.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 33;
+    int b = 15;
+    int result = 0;
+
+    result = a + b;
+    // result = a - b;
+    // result = a * b;
+    // result = a / b;
+    // result = a % b;
+
+    printf("C is %d \n", result );      // 48
+
+    printf("%d\n", --a);        // 32
+    printf("%d\n", ++b);        // 16
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
