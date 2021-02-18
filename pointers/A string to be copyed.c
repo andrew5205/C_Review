c0ce2e2491f8a208730d217de91e028820648c3c
Andrew5205 <andrewchung11@gmail.com>
2021-02-17 20:00:21 -0800

    add A string to be copyed.c

---
+++ pointers/A string to be copyed.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <string.h>
+
+
+void copyString(char *to, char *from)
+{
+    while( *from )       // *from pointer not point to NULL
+        *to++ = *from++;
+    
+    *to = '\0';
+}
+
+
+
+int main(void)
+{
+    char string1[] = "A string to be copyed";
+    char string2[50];
+
+    copyString(string2, string1);
+    printf("%s\n", string2);        // A string to be copyed
+    return 0;
+}
+
+
+
+
